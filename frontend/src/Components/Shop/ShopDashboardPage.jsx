fd35c0575706c5dbf5a376e620b65748fb0c14b5
Robinkhilery960 <robinkhilery960@gmail.com>
2024-02-27 17:39:44 +0530

    Shop dashboard hero page done

--- frontend/src/Components/Shop/ShopDashboardPage.jsx
+++ frontend/src/Components/Shop/ShopDashboardPage.jsx
@@ -1,17 +1,19 @@
 import React, { useState } from 'react'
 import DashboardHeader from "../../Components/Shop/Layout/DashboardHeader.jsx"
 import DashboardSideBar from "../../Components/Shop/Layout/DashboardSideBar.jsx"
+import DashboardHero from "../../Components/Shop/Layout/DashboardHero.jsx"
 
 const ShopDashboardPage = () => {
-     
+
     return (
-        <div> 
+        <div>
             <>
-                <DashboardHeader/>
-                <div className="flex items-center justify-between w-full">
+                <DashboardHeader />
+                <div className="flex items-start  justify-between w-full">
                     <div className="w-[100px] 800px:w-[330px]">
                         <DashboardSideBar active={1} />
                     </div>
+                    <DashboardHero />
                 </div>
             </>
         </div>
