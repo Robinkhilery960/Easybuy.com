eed86c4fc9e4176d24319cbfbbc049496bd3ef72
Robin Khilery <96528134+Robinkhilery960@users.noreply.github.com>
2024-03-02 07:00:07 +0530

    cookie option changed

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -3,12 +3,10 @@ const sendToken=async(user, statusCode, res)=>{
     const token= await user.getJwtToken(); 
     // option for cookies
     const options={
-        expires:new Date(Date.now()+90*24*60*60*1000),
-        httpOnly:true, 
+        expires:new Date(Date.now()+90*24*60*60*1000), 
+        httpOnly:true,
         sameSite:"none",
-        secure:true
-    }
-
+        secure:true }
     res.status(statusCode).cookie("token", token , options).json({
         sucess:true,
         user,
